9241231acfc3c902fd8158c4826e8e7317d7b794
Shemar Dacosta <dshemar6@gmail.com>
2019-01-24 06:53:26 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,53 @@
-def reformat_languages(languages)
-  # your code here
+require "pry"
+
+def languages
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+}
 end
+
+def reformat_languages(languages)
+  new_hash = {}
+  languages.each do |language, data|
+    data.each do |lang_name, info|
+      info.each do |type, value|
+        if new_hash.key?(lang_name) == false
+          new_hash[lang_name] = {type => value, :style => [language]}
+        else
+          new_hash[lang_name][:style] << language
+        end
+      end
+    end
+  end
+  new_hash
+end
+
+reformat_languages(languages)
